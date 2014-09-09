04d055830a434305cc6b45ee61eff2bc68e5d433
Erwin Poeze <erwin@mope.mobi>
2014-09-09 17:33:48 +0200

    some cleaning up

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Type definitions for [Ramda](https://github.com/CrossEye/ramda).
 
+Test with:
+```
+tsc --module amd ramda-tests.ts
+```
+
 Will be pushed to [DefinitelyTyped](https://github.com/borisyankov/DefinitelyTyped) when finished.
--- ramda.d.ts
+++ ramda.d.ts
@@ -1,161 +1,238 @@
-declare module Helpers {
-  interface ListIterator<T, TResult> {
-    (value: T, index: number, list: T[]): TResult;
-  }
+declare module H {
+    interface ListIterator<T, TResult> {
+        (value: T, index: number, list: T[]): TResult;
+    }
 
-  interface List<T> {
-    [index: number]: T;
-    length: number;
-  }
+    interface List<T> {
+        [index: number]: T;
+        length: number;
+    }
 
-  interface ArrayLike {
-      nodeType: number;
-  }
+    interface ArrayLike {
+        nodeType: number;
+    }
 
-  interface FuncIndex extends Function {
-    (): any;
-    idx: Function;
-  }
+    interface FuncIndex extends Function {
+        (): any;
+        idx: Function;
+    }
 }
 
 interface RamdaStatic {
+    /**
+     * returns a subset of a string between one index and another.
+     *
+     * @func
+     * @memberOf R
+     * @category string
+     * @sig Number -> Number -> String -> String
+     * @param {Number} indexA An integer between 0 and the length of the string.
+     * @param {Number} indexB An integer between 0 and the length of the string.
+     * @param {String} The string to extract from
+     * @return {String} the extracted substring
+     * @see R.invoker
+     * @example
+     *
+     *      substring(2, 5, 'abcdefghijklm'); //=> 'cde'
+     */
+    substring(
+            indexA: number,
+            indexB: number,
+            The: string): string;
 
-  substring(
-      indexA: number,
-      indexB: number,
-      The: string): string;
+    /**
+     * The trailing substring of a String starting with the nth character:
+     *
+     * @func
+     * @memberOf R
+     * @category string
+     * @sig Number -> String -> String
+     * @param {Number} indexA An integer between 0 and the length of the string.
+     * @param {String} The string to extract from
+     * @return {String} the extracted substring
+     * @see R.invoker
+     * @example
+     *
+     *      substringFrom(8, 'abcdefghijklm'); //=> 'ijklm'
+     */
+    substringFrom(
+            indexA: number,
+            The: string): string;
 
-  substringFrom(
-      indexA: number,
-      The: string): string;
+    /**
+     * The leading substring of a String ending before the nth character:
+     *
+     * @func
+     * @memberOf R
+     * @category string
+     * @sig Number -> String -> String
+     * @param {Number} indexA An integer between 0 and the length of the string.
+     * @param {String} The string to extract from
+     * @return {String} the extracted substring
+     * @see R.invoker
+     * @example
+     *
+     *      substringTo(8, 'abcdefghijklm'); //=> 'abcdefgh'
+     */
+    substringTo(
+            indexA: number,
+            The: string): string;
 
-  substringTo(
-      indexA: number,
-      The: string): string;
+    toString(): string;
 
-  toString(): string;
+    /**
+     * Tests whether or not an object is an array.
+     *
+     * @private
+     * @category Internal
+     * @param {*} val The object to test.
+     * @return {boolean} `true` if `val` is an array, `false` otherwise.
+     * @example
+     *
+     *      isArray([]); //=> true
+     *      isArray(true); //=> false
+     *      isArray({}); //=> false
+     */
+    // isArray(
+    //         a: any[]): boolean;
+    //
+    // isArrayLike(
+    //         a: Array<any>): boolean;
+    //
+    // isArrayLike(
+    //         a: ArrayLike): boolean;
 
-  isArray(
-      a: any[]): boolean;
+    /**
+     * Creates a new version of `fn` that, when invoked, will return either:
+     * - A new function ready to accept one or more of `fn`'s remaining arguments, if all of
+     * `fn`'s expected arguments have not yet been provided
+     * - `fn`'s result if all of its expected arguments have been provided
+     *
+     * Optionally, you may provide an arity for the returned function.
+     *
+     * @func
+     * @memberOf R
+     * @category Function
+     * @sig (* -> a) -> Number -> (* -> a)
+     * @sig (* -> a) -> (* -> a)
+     * @param {Function} fn The function to curry.
+     * @param {number} [fnArity=fn.length] An optional arity for the returned function.
+     * @return {Function} A new, curried function.
+     * @example
+     *
+     *      var addFourNumbers = function(a, b, c, d) {
+     *        return a + b + c + d;
+     *      };
+     *
+     *      var curriedAddFourNumbers = curry(addFourNumbers);
+     *      var f = curriedAddFourNumbers(1, 2);
+     *      var g = f(3);
+     *      g(4);//=> 10
+     */
+    curry(
+            fn: (...args: any[]) => any,
+            fnArity?: number): any;
 
-  isArrayLike(
-      a: Array<any>): boolean;
+    nAry(
+            a: number,
+            fn: () => any): any;
+    nAry(
+            a: number,
+            fn: (arg0: any, ...rest: any[]) => any): any;
 
-  isArrayLike(
-      a: ArrayLike): boolean;
+    arity(
+            a: number,
+            fn: Function): any;
+    arity(
+            a: number,
+            fn: (...args: any[]) => any): any;
 
-  curry(
-      fn: (...args: any[]) => any,
-      fnArity?: number): any;
+    unary(
+            fn: Function): any;
 
-  nAry(
-      a: number,
-      fn: () => any): any;
-  nAry(
-      a: number,
-      fn: (arg0: any, ...rest: any[]) => any): any;
+    unary(
+            fn: (...args: any[]) => any): any;
 
-  arity(
-      a: number,
-      fn: Function): any;
-  arity(
-      a: number,
-      fn: (...args: any[]) => any): any;
+    binary(
+            fn: Function): any;
 
-  unary(
-      fn: Function): any;
+    binary(
+            fn: (...args: any[]) => any): any;
 
-  unary(
-      fn: (...args: any[]) => any): any;
+    invoker(
+            name: string,
+            obj: any,
+            len?: number): any;
 
-  binary(
-      fn: Function): any;
+    useWith(
+            fn: Function,
+            ...rest: Function[]): any;
 
-  binary(
-      fn: (...args: any[]) => any): any;
+    forEach<T>(
+            fn: H.ListIterator<T, void>,
+            list: H.List<T>): H.List<T>;
 
-  invoker(
-      name: string,
-      obj: any,
-      len?: number): any;
+    clone<T>(
+            list: H.List<T>): H.List<T>;
 
-  useWith(
-      fn: Function,
-      ...rest: Function[]): any;
 
-  forEach<T>(
-      fn: ListIterator<T, void>,
-      list: List<T>): List<T>;
+    isEmpty<T>(
+            list: H.List<T>): boolean;
 
-  clone<T>(
-      list: List<T>): List<T>
+    prepend<T>(
+            el: T,
+            list: H.List<T>): H.List<T>;
+    cons<T>(
+            el: T,
+            list: H.List<T>): H.List<T>;
 
-  // Core functions
+    head<T>(list: H.List<T>): H.List<T>;
+    car<T>(list: H.List<T>): H.List<T>;
 
-  isEmpty<T>(
-      list: List<T>): boolean;
+    last<T>(list: H.List<T>): H.List<T>;
 
-  prepend<T>(
-      el: T,
-      list: List<T>): List<T>;
-  cons<T>(
-      el: T,
-      list: List<T>): List<T>;
+    tail<T>(list: H.List<T>): H.List<T>;
+    cdr<T>(list: H.List<T>): H.List<T>;
 
-  head<T>(list: List<T>): List<T>;
-  car<T>(list: List<T>): List<T>;
+    isAtom(x: any): boolean;
 
-  last<T>(list: List<T>): List<T>;
+    append<T>(
+            el: T,
+            list: H.List<T>): H.List<T>;
+    push<T>(
+            el: T,
+            list: H.List<T>): H.List<T>;
 
-  tail<T>(list: List<T>): List<T>;
-  cdr<T>(list: List<T>): List<T>;
+    concat<T>(
+            list1: H.List<T>,
+            list2: H.List<T>): H.List<T>;
 
-  isAtom(x: any): boolean;
+    identity(a: any): any;
+    I(a: any): any;
 
-  append<T>(
-      el: T,
-      list: List<T>): List<T>;
-  push<T>(
-      el: T,
-      list: List<T>): List<T>;
+    times(
+            fn: (i: number) => any,
+            n: number): H.List<any>
 
-  concat<T>(
-      list1: List<T>,
-      list2: List<T>): List<T>;
+                        repeatN(
+                                value: number,
+                                n: number): any[];
 
-  identity(a: any): any;
-  I(a: any): any;
+    compose(
+            ...Functions: Function[]): Function;
 
-  times(
-      fn: (i: number) => any,
-      n: number): List<any>
+    pipe(
+            ...Functions: Function[]): Function;
 
-  repeatN(
-      value: number,
-      n: number): any[];
-
-  compose(
-      ...Functions: Function[]): Function;
-
-  pipe(
-      ...Functions: Function[]): Function;
-
-  flip(
-      fn: (arg0: any, ...args: any[]) => any): Function
+    flip(
+            fn: (arg0: any, ...args: any[]) => any): Function
 }
 
 declare var R: RamdaStatic;
 declare module 'ramda' {
-  export = R;
+    export = R;
 }
 
-declare function forEach<T>(
-      fn: ListIterator<T, void>,
-      list: List<T>): List<T>;
-declare module 'ramda' {
-  export interface forEach<T>
-  var idx: Function
-}
 
 
 
