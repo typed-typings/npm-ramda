437b0d5b9a03fb7797afc0ba89d9362ee7052b90
Uros Smolnik <uros.smolnik@gmail.com>
2016-03-30 09:45:52 -0700

    Fix for merge - use type intersection

--- ramda.d.ts
+++ ramda.d.ts
@@ -760,8 +760,9 @@ declare module R {
          * merged with the own properties of object b.
          * This function will *not* mutate passed-in objects.
          */
-        merge(a: any, b: any): any;
-        merge(a: any): (b: any) => any;
+        merge<T1, T2>(a: T1, b: T2): T1 & T2;
+        merge<T1>(a: T1): <T2>(b: T2) => T1 & T2;
+
 
         /**
          * Merges a list of objects together into one object.
