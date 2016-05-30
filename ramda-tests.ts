21164bfa6e9a9fde3111c0a96c691ff326d72fad
Erwin Poeze <erwin@mope.mobi>
2016-05-30 14:27:49 +0200

    adds test for cond()

--- ramda-tests.ts
+++ ramda-tests.ts
@@ -847,6 +847,17 @@ type Pair = KeyValuePair<string, number>
 }
 
 () => {
+    const fn = R.cond([
+        [R.equals(0),   R.always('water freezes at 0°C')],
+        [R.equals(100), R.always('water boils at 100°C')],
+        [R.T,           (temp: number) => 'nothing special happens at ' + temp + '°C']
+    ]);
+    const a: string = fn(0); //=> 'water freezes at 0°C'
+    const b: string = fn(50); //=> 'nothing special happens at 50°C'
+    const c: string = fn(100); //=> 'water boils at 100°C'
+}
+
+() => {
     R.tail(['fi', 'fo', 'fum']); //=> ['fo', 'fum']
     R.tail([1, 2, 3]); //=> [2, 3]
 }
