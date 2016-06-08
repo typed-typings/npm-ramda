bc6c44a281b3fc44633a61591e11943e7c042582
Erwin Poeze <erwin@mope.mobi>
2016-06-08 10:44:03 +0200

    alternative typing of evolve

--- ramda-tests.ts
+++ ramda-tests.ts
@@ -1057,6 +1057,16 @@ type Pair = KeyValuePair<string, number>
     const a2 = R.evolve({ elapsed: R.add(1), remaining: R.add(-1) })({ name: 'Tomato', elapsed: 100, remaining: 1400 });
 }
 
+() => {
+    var tomato = {firstName: 'Tomato ', data: {elapsed: 100, remaining: 1400}, id:123};
+    var transformations = {
+        firstName: R.trim,
+        lastName: R.trim, // Will not get invoked.
+        data: {elapsed: R.add(1), remaining: R.add(-1)}
+    };
+    const a = R.evolve(transformations, tomato); //=> {firstName: 'Tomato', data: {elapsed: 101, remaining: 1399}, id:123}
+    const b = R.evolve(transformations)(tomato); //=> {firstName: 'Tomato', data: {elapsed: 101, remaining: 1399}, id:123}
+}
 
 () => {
     const hasName = R.has('name');
