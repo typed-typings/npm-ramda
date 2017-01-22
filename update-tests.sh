3b83e7079772c970a5b770c7f5d32dad3ac24dbc
tycho01 <tychogrouwstra@gmail.com>
2017-01-23 00:47:03 +0800

    fix double colon, typings-checker options: --verbose --noLines

--- update-tests.sh
+++ update-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for test in $(find tests -name '*.ts'); do
   # if [[ $(uname) == MS* ]]; then # Windows (PowerShell)
-    # node ./node_modules/typings-checker/src/index.js $test 2>&1 | Out-File -encoding utf8 tee $test.out
+    # node ./node_modules/typings-checker/src/index.js $test --verbose --noLines 2>&1 | Out-File -encoding utf8 tee $test.out
   if [[ $PWD == /mnt/* ]]; then # Bash on Windows, messes with local path so try global install
-    typings-checker $test 2>&1 | tee $test.out
+    typings-checker $test --verbose --noLines 2>&1 | tee $test.out
   else
-    node ./node_modules/typings-checker/src/index.js $test 2>&1 | tee $test.out
+    node ./node_modules/typings-checker/src/index.js $test --verbose --noLines 2>&1 | tee $test.out
   fi
   rc=${PIPESTATUS[0]}; if [[ $rc != 0 ]]; then exit $rc; fi
 done
