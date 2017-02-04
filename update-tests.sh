8fe205c35e19eff837566c5629c9bb11c4352cac
tycho01 <tychogrouwstra@gmail.com>
2017-02-04 23:14:21 +0800

    adjustments for official typings-checker...
    can't use for code fragments instead of line numbers, so kind of sucks
    for our use-case :(

--- update-tests.sh
+++ update-tests.sh
@@ -3,9 +3,9 @@ for test in $(find tests -name '*.ts'); do
   # if [[ $(uname) == MS* ]]; then # Windows (PowerShell)
     # node ./node_modules/typings-checker/dist/index.js $test --verbose --noLines 2>&1 | Out-File -encoding utf8 tee $test.out
   if [[ $PWD == /mnt/* ]]; then # Bash on Windows, messes with local path so try global install
-    typings-checker $test --verbose --noLines 2>&1 | tee $test.out
+    ./node_modules/typings-checker/dist/index.js --allow-expect-error --project tests/tsconfig.json $test 2>&1 | perl -pe 's/\.ts:(\d+)/.ts/' | tee $test.out
   else
-    node ./node_modules/typings-checker/dist/index.js $test --verbose --noLines 2>&1 | tee $test.out
+    node ./node_modules/typings-checker/dist/index.js --allow-expect-error --project tests/tsconfig.json $test 2>&1 | perl -pe 's/\.ts:(\d+)/.ts/' | tee $test.out
   fi
   rc=${PIPESTATUS[0]}; if [[ $rc != 0 ]]; then exit $rc; fi
 done
