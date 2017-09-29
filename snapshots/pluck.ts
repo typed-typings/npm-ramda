e84cdca07f10b6f2c541ee3e2a4cf3df928d80ab
ikatyang <ikatyang@gmail.com>
2017-09-29 13:01:33 +0800

    test: change test case

--- snapshots/pluck.ts
+++ snapshots/pluck.ts
@@ -4,10 +4,10 @@ import R_pluck = require('../ramda/dist/src/pluck');
 declare const placeholder: Placeholder;
 declare const a_number_record: Record<'a', number>;
 declare const string_array: string[];
-declare const x_y_z_with_a_1_record: {
+declare const x_y_z_with_a_record: {
   x: { a: 1 };
-  y: { a: 1 };
-  z: { a: 1 };
+  y: { a: true };
+  z: { a: 'string' };
 };
 
 // @dts-jest:pass:snap -> <V, T extends Record<"a", V>>(list: T[] | ArrayLike<T>) => T["a"][]
@@ -23,8 +23,8 @@ R_pluck(placeholder, [a_number_record, a_number_record]);
 // @dts-jest:pass:snap -> number[]
 R_pluck(placeholder, [a_number_record, a_number_record])('a');
 
-// @dts-jest:pass:snap -> Record<"x" | "y" | "z", 1>
-R_pluck('a', x_y_z_with_a_1_record);
+// @dts-jest:pass:snap -> Record<"x" | "y" | "z", true | "string" | 1>
+R_pluck('a', x_y_z_with_a_record);
 
 // @dts-jest:pass:snap -> string[]
 R_pluck(0, [string_array]);
--- tests/pluck.ts
+++ tests/pluck.ts
@@ -4,10 +4,10 @@ import R_pluck = require('../ramda/dist/src/pluck');
 declare const placeholder: Placeholder;
 declare const a_number_record: Record<'a', number>;
 declare const string_array: string[];
-declare const x_y_z_with_a_1_record: {
+declare const x_y_z_with_a_record: {
   x: { a: 1 };
-  y: { a: 1 };
-  z: { a: 1 };
+  y: { a: true };
+  z: { a: 'string' };
 };
 
 // @dts-jest:pass:snap
@@ -24,7 +24,7 @@ R_pluck(placeholder, [a_number_record, a_number_record]);
 R_pluck(placeholder, [a_number_record, a_number_record])('a');
 
 // @dts-jest:pass:snap
-R_pluck('a', x_y_z_with_a_1_record);
+R_pluck('a', x_y_z_with_a_record);
 
 // @dts-jest:pass:snap
 R_pluck(0, [string_array]);
