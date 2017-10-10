11a6082f7c4575a1b26c1c87ca9d17727238c06e
Ika <ikatyang@gmail.com>
2017-10-10 17:27:49 +0800

    fix(adjust): add tuple kind

--- templates/adjust.d.ts
+++ templates/adjust.d.ts
@@ -1,7 +1,19 @@
 import { List, Morphism } from './$types';
 
-export function $<T, U>(
+export function $list<T, U>(
   fn: Morphism<T, U>,
   index: number,
   list: List<T>,
 ): Array<T | U>;
+
+export function $tuple<N extends number, X extends [any]>(
+  fn: Morphism<X[N], X[N]>,
+  index: N,
+  tuple: X,
+): X;
+
+export function $general<T, U, X extends [any]>(
+  fn: Morphism<T, U>,
+  index: number,
+  list: List<T> | X,
+): Array<T | U> | X;
--- tests/adjust.ts
+++ tests/adjust.ts
@@ -4,6 +4,7 @@ import R_adjust = require('../ramda/dist/src/adjust');
 declare const string_to_string: Morphism<string, string>;
 declare const string_to_number: Morphism<string, number>;
 declare const string_array: string[];
+declare const string_number_tuple: [string, number];
 declare const number: number;
 
 // @dts-jest:pass:snap
@@ -18,3 +19,8 @@ R_adjust(string_to_string)(number, string_array);
 R_adjust(string_to_string, number)(string_array);
 // @dts-jest:pass:snap
 R_adjust(string_to_string, number, string_array);
+
+// @dts-jest:pass:snap
+R_adjust<'111', 'tuple'>()(string_to_string, 0, string_number_tuple);
+// @dts-jest:fail:snap
+R_adjust<'111', 'tuple'>()(string_to_string, 1, string_number_tuple);
