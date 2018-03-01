66b42b067f0329747ba9ddeed41a7eb3be072085
Ika <ikatyang@gmail.com>
2018-03-01 13:53:50 +0800

    feat(equals): add void cases

--- templates/equals.d.ts
+++ templates/equals.d.ts
@@ -1,1 +1,3 @@
-export function $<T>(a: T, b: T): boolean;
+export function $null<T>(a: null, b: T | null): boolean;
+export function $undefined<T>(a: undefined, b: T | undefined): boolean;
+export function $general<T>(a: T, b: T): boolean;
