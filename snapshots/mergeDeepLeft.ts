20b3d8350384763f57763c2b169a87d0bda44e70
Ika <ikatyang@gmail.com>
2018-08-15 00:36:33 +0800

    test: update snapshots

--- snapshots/mergeDeepLeft.ts
+++ snapshots/mergeDeepLeft.ts
@@ -5,7 +5,7 @@ declare const b_2: { b: 2 };
 declare const a_1_x_y_1: { a: 1; x: { y: 1 } };
 declare const b_1_x_z_1: { b: 1; x: { z: 1 } };
 
-// @dts-jest:pass:snap -> <U extends object>(right: U) => { "0": Pick<U, Exclude<keyof U, "a">> & Pick<{ a: 1; }, Exclude<"a", keyof U>> & { [K in Extract<keyof U, "a">]: any[U[K] extends object ? { a: 1; }[K] extends object ? 0 : 1 : 1]; }; "1": { a: 1; }; }[U extends object ? 0 : 1]
+// @dts-jest:pass:snap -> <U extends object>(right: U) => { 0: Pick<U, Exclude<keyof U, "a">> & Pick<{ a: 1; }, Exclude<"a", keyof U>> & { [K in Extract<keyof U, "a">]: any[U[K] extends object ? { a: 1; }[K] extends object ? 0 : 1 : 1]; }; 1: { a: 1; }; }[U extends object ? 0 : 1]
 R.mergeDeepLeft(a_1);
 
 // @dts-jest:pass:snap -> Pick<{ b: 2; }, "b"> & Pick<{ a: 1; }, "a"> & {}
--- snapshots/mergeDeepRight.ts
+++ snapshots/mergeDeepRight.ts
@@ -5,7 +5,7 @@ declare const b_2: { b: 2 };
 declare const a_1_x_y_1: { a: 1; x: { y: 1 } };
 declare const b_1_x_z_1: { b: 1; x: { z: 1 } };
 
-// @dts-jest:pass:snap -> <U extends object>(right: U) => { "0": Pick<{ a: 1; }, Exclude<"a", keyof U>> & Pick<U, Exclude<keyof U, "a">> & { [K in Extract<"a", keyof U>]: any[{ a: 1; }[K] extends object ? U[K] extends object ? 0 : 1 : 1]; }; "1": U; }[U extends object ? 0 : 1]
+// @dts-jest:pass:snap -> <U extends object>(right: U) => { 0: Pick<{ a: 1; }, Exclude<"a", keyof U>> & Pick<U, Exclude<keyof U, "a">> & { [K in Extract<"a", keyof U>]: any[{ a: 1; }[K] extends object ? U[K] extends object ? 0 : 1 : 1]; }; 1: U; }[U extends object ? 0 : 1]
 R.mergeDeepRight(a_1);
 
 // @dts-jest:pass:snap -> Pick<{ a: 1; }, "a"> & Pick<{ b: 2; }, "b"> & {}
